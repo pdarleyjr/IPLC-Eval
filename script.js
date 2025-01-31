11690ae0198e46abffe2b1b441c50ec8a39016c3
pdarleyjr <pdarleyjr@gmail.com>
2025-01-31 11:40:26 -0500

    Update T5 API request format to include fn_index

--- script.js
+++ script.js
@@ -184,6 +184,7 @@ document.addEventListener('DOMContentLoaded', function() {
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify({
+                        fn_index: 0,
                         data: [prompt]
                     })
                 });
